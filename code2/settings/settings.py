d7ac953078cb26d1f73b8bb1253028fcb35ae976
ksrinivas007 <sreeni_kand@yahoo.com>
2020-09-22 15:49:56 +0100

    settings

---
+++ code2/settings/settings.py
@@ -0,0 +1,11 @@
+
+# Base url
+BASE_URL = "https://www.nsai.ie/standards/search-buy-standards/new-irish-standards-2020/"
+
+# Headers to be passed on the request module
+BROWSER_HEADERS = ("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/53 (KHTML, like Gecko) Chrome/15.0.87")
+
+# PDFs folder
+PDF_FOLDER = "code2/pdf/"
+CSV_FOLDER = "code2/csv/"
+
